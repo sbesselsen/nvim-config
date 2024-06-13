39238f826c65245c4cf0e56625f99cc111f1ee9c
Sebastiaan Besselsen <sebastiaan.besselsen@gmail.com>
2024-06-13 21:29:17 +0200

    Add conform + mason, format

--- init.lua
+++ init.lua
@@ -7,8 +7,7 @@ vim.g.loaded_netrwPlugin = 1
 require("core.lazy")
 require("core.mappings")
 
-vim.cmd.colorscheme "catppuccin-frappe"
+vim.cmd.colorscheme("catppuccin-frappe")
 
 vim.wo.number = true
 vim.wo.relativenumber = true
-
--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,10 +1,19 @@
 vim.keymap.set("n", "+", ":Neotree toggle<CR>")
 vim.keymap.set("n", "<C-l>", ":set invrelativenumber<CR>", { noremap = true, desc = "Toggle relative line numbers" })
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find files" })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Live grep" })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Buffers" })
-vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = "Buffers" })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Help tags" })
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+vim.keymap.set("n", "<leader><leader>", builtin.find_files, { desc = "Find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
+vim.keymap.set("n", "<leader>b", builtin.buffers, { desc = "Buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
 
+vim.keymap.set({ "n", "v" }, "<leader>cf", function()
+	local conform = require("conform")
+	conform.format({
+		lsp_fallback = true,
+		async = false,
+		timeout_ms = 500,
+	})
+end, { desc = "Format file or range (in visual mode)" })
